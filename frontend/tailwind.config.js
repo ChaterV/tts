ee4984c3b077d8ae8afc4b5915e1f7d68022292f
chaterV <455699514@qq.com>
2025-08-14 10:45:22 +0800

    重构

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+    darkMode: 'class', // 启用类模式来控制深色/浅色主题
+    content: [
+        "./index.html",
+        "./src/**/*.{js,ts,jsx,tsx}",
+    ],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+}
